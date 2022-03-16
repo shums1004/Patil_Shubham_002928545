b644afa8793884da8e524cf9298c01b2d8a6baf3
shums1004 <patil.shub@northeastern.edu>
2022-03-16 19:54:18 -0400

    Assignment 4

---
+++ Assignment-4/src/model/VitalsDirectory.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author shubhampatil
+ */
+public class VitalsDirectory {
+    
+    
+    private ArrayList<Vitals>  vdirectory;
+    
+    public VitalsDirectory() {
+
+        this.vdirectory = new ArrayList<Vitals>();
+        
+}
+
+    public ArrayList<Vitals> getVdirectory() {
+        return vdirectory;
+    }
+
+    public void setVdirectory(ArrayList<Vitals> vdirectory) {
+        this.vdirectory = vdirectory;
+    }
+    
+    
+    public Vitals addNewVitals() {
+        
+        Vitals newVitals = new Vitals();
+        
+        vdirectory.add(newVitals);
+        
+        return newVitals;
+        
+    }
+}
+
+
